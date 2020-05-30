5c26b8abfb650c3e176fa84a4440047129f3e809
manowski <manowski10@gmail.com>
2020-05-30 22:16:55 +0200

    Add solution for task1 in 2-logic-tests

--- 2-logic-tests/src/task1/index.js
+++ 2-logic-tests/src/task1/index.js
@@ -4,6 +4,12 @@ export const getBalanceByCategoryInPeriod = (
   startTime,
   endTime
 ) => {
-  // Add your implementation here...
-  return 0;
+
+  if (!transactionsList.length) return 0
+
+  const total = transactionsList
+    .filter(trans => trans.category === category && startTime <= new Date(trans.time) && endTime >= new Date(trans.time))
+    .reduce((total, num) => total + num.amount, 0)
+
+  return total
 }
--- 2-logic-tests/src/task1/index.test.js
+++ 2-logic-tests/src/task1/index.test.js
@@ -32,5 +32,108 @@ describe('Task1', () => {
     expect(result).toEqual(0);
   });
 
-  // Add more tests here...
+  const INPUT = [
+    {
+      id: 1,
+      sourceAccount: 'my_account',
+      targetAccount: 'coffee_shop',
+      amount: 50,
+      category: 'groceries',
+      time: '2018-03-02T10:34:30.000Z'
+    },
+    {
+      id: 2,
+      sourceAccount: 'my_account',
+      targetAccount: 'my_account',
+      amount: 10,
+      category: 'others',
+      time: '2018-03-02T10:34:30.000Z'
+    },
+    {
+      id: 3,
+      sourceAccount: 'my_account',
+      targetAccount: 'coffee_shop',
+      amount: 30,
+      category: 'groceries',
+      time: '2018-03-03T10:34:30.000Z'
+    },
+    {
+      id: 4,
+      sourceAccount: 'my_account',
+      targetAccount: 'coffee_shop',
+      amount: -50,
+      category: 'groceries',
+      time: '2018-03-05T10:34:30.000Z'
+    },
+    {
+      id: 5,
+      sourceAccount: 'my_account',
+      targetAccount: 'coffee_shop',
+      amount: -60,
+      category: 'groceries',
+      time: '2018-03-05T10:34:30.000Z'
+    },
+    {
+      id: 6,
+      sourceAccount: 'my_account',
+      targetAccount: 'my_account',
+      amount: -60,
+      category: 'others',
+      time: '2018-03-06T10:34:30.000Z'
+    },
+    {
+      id: 7,
+      sourceAccount: 'my_account',
+      targetAccount: 'coffee_shop',
+      amount: 100,
+      category: 'others',
+      time: '2018-03-07T10:34:30.000Z'
+    }
+  ]
+
+  it('returns 80 in groceries category in selected date period', () => {
+    const result = getBalanceByCategoryInPeriod(
+      INPUT,
+      'groceries',
+      new Date('2018-03-01'),
+      new Date('2018-03-04')
+    );
+
+    expect(result).toEqual(80);
+  });
+
+  it('returns -80 in groceries category in selected date period', () => {
+    const result = getBalanceByCategoryInPeriod(
+      INPUT,
+      'groceries',
+      new Date('2018-03-03'),
+      new Date('2018-03-06')
+    );
+
+    expect(result).toEqual(-80);
+  });
+
+  it('returns 0 on non-matching category', () => {
+    const result = getBalanceByCategoryInPeriod(
+      INPUT,
+      'category',
+      new Date('2018-03-03'),
+      new Date('2018-03-06')
+    );
+
+    expect(result).toEqual(0);
+  });
+
+  it('returns 0 if startTime is greater than endTime', () => {
+    const result = getBalanceByCategoryInPeriod(
+      INPUT,
+      'groceries',
+      new Date('2018-03-12'),
+      new Date('2018-03-02')
+    );
+
+    expect(result).toEqual(0);
+  });
+
+
 });
