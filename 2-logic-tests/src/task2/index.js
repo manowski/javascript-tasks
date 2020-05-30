9099586e640a16678a68a5a7e3235dfc8780ef0a
manowski <manowski10@gmail.com>
2020-05-30 22:18:14 +0200

    Add solution for task2 in 2-logic-tests

--- 2-logic-tests/src/task2/index.js
+++ 2-logic-tests/src/task2/index.js
@@ -1,4 +1,52 @@
 export const findDuplicateTransactions = (transactions = []) => {
-  // Add your implementation here...
-  return [];
+
+  transactions = transactions.sort(calculateTime)
+  const duplicatedTrans = []
+  const minute = 60000
+
+  // groups transactions based on their source, target, category and amount
+  const similarTrans = transactions.reduce((total, trans) => {
+    const key = trans.sourceAccount + trans.targetAccount + trans.category + trans.amount
+    total[key] = [...(total[key] || []), trans]
+    return total
+  }, {})
+
+  Object.keys(similarTrans).forEach(key => {
+    let previousTransaction   // holds previous transaction
+    let duplicates = [] // holds duplicates of group
+
+    similarTrans[key].forEach((txn, item) => {
+      if (previousTransaction) {
+        const endTime = new Date(previousTransaction.time)
+        const startTime = new Date(txn.time)
+
+        if (startTime - endTime <= minute) {
+          duplicates = duplicates.length === 0 ? [previousTransaction, txn] : [...duplicates, txn]
+        } else if (duplicates.length) {
+          duplicatedTrans.push(duplicates)
+          duplicates = []
+        }
+      }
+
+      // if transaction is last of group, send duplicates to the list
+      if (similarTrans[key].length === item + 1 && duplicates.length) {
+        duplicatedTrans.push(duplicates)
+      }
+
+      // saves transaction for next iteration
+      previousTransaction = txn
+    })
+  })
+
+  // sorts duplicates by their first transaction time
+  return duplicatedTrans.sort((a1, a2) => {
+    return calculateTime(a1[0], a2[0])
+  })
+}
+
+
+const calculateTime = (time1, time2) => {
+  const m1 = new Date(time1.time)
+  const m2 = new Date(time2.time)
+  return m1 >= m2 ? 1 : -1
 }
--- 2-logic-tests/src/task2/index.test.js
+++ 2-logic-tests/src/task2/index.test.js
@@ -1,5 +1,6 @@
 import { findDuplicateTransactions } from './';
 
+
 describe('Task2', () => {
   it('returns empty array if there are no transactions', () => {
     expect(findDuplicateTransactions([])).toEqual([]);
@@ -61,4 +62,197 @@ describe('Task2', () => {
   });
 
   // Add more tests here...
+  it('compare input and output', () => {
+    const INPUT = [
+      {
+        id: 3,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:34:30.000Z'
+      },
+      {
+        id: 1,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:33:00.000Z'
+      },
+      {
+        id: 6,
+        sourceAccount: 'A',
+        targetAccount: 'C',
+        amount: 250,
+        category: 'other',
+        time: '2018-03-02T10:33:05.000Z'
+      },
+      {
+        id: 4,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:36:00.000Z'
+      },
+      {
+        id: 2,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:33:50.000Z'
+      },
+      {
+        id: 5,
+        sourceAccount: 'A',
+        targetAccount: 'C',
+        amount: 250,
+        category: 'other',
+        time: '2018-03-02T10:33:00.000Z'
+      }
+    ];
+    const OUTPUT = [
+      [
+        {
+          id: 1,
+          sourceAccount: "A",
+          targetAccount: "B",
+          amount: 100,
+          category: "eating_out",
+          time: "2018-03-02T10:33:00.000Z"
+        },
+        {
+          id: 2,
+          sourceAccount: "A",
+          targetAccount: "B",
+          amount: 100,
+          category: "eating_out",
+          time: "2018-03-02T10:33:50.000Z"
+        },
+        {
+          id: 3,
+          sourceAccount: "A",
+          targetAccount: "B",
+          amount: 100,
+          category: "eating_out",
+          time: "2018-03-02T10:34:30.000Z"
+        }
+      ],
+      [
+        {
+          id: 5,
+          sourceAccount: "A",
+          targetAccount: "C",
+          amount: 250,
+          category: "other",
+          time: "2018-03-02T10:33:00.000Z"
+        },
+        {
+          id: 6,
+          sourceAccount: "A",
+          targetAccount: "C",
+          amount: 250,
+          category: "other",
+          time: "2018-03-02T10:33:05.000Z"
+        }
+      ]
+    ];
+
+    expect(findDuplicateTransactions(INPUT)).toEqual(OUTPUT);
+  });
+
+  it('returns empty array if the category is not the same', () => {
+    const INPUT = [
+      {
+        id: 1,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:33:00.000Z'
+      },
+      {
+        id: 2,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating',
+        time: '2018-03-02T10:32:00.000Z'
+      }
+    ];
+
+    expect(findDuplicateTransactions(INPUT)).toEqual([]);
+  });
+
+  it('returns empty array if the sourceAccount is not the same', () => {
+    const INPUT = [
+      {
+        id: 1,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:33:00.000Z'
+      },
+      {
+        id: 2,
+        sourceAccount: 'B',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:32:00.000Z'
+      }
+    ];
+
+    expect(findDuplicateTransactions(INPUT)).toEqual([]);
+  });
+
+  it('returns empty array if the targetAccount is not the same', () => {
+    const INPUT = [
+      {
+        id: 1,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:33:00.000Z'
+      },
+      {
+        id: 2,
+        sourceAccount: 'A',
+        targetAccount: 'C',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:32:00.000Z'
+      }
+    ];
+
+    expect(findDuplicateTransactions(INPUT)).toEqual([]);
+  });
+
+  it('returns empty array if the amount is not the same', () => {
+    const INPUT = [
+      {
+        id: 1,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 100,
+        category: 'eating_out',
+        time: '2018-03-02T10:33:00.000Z'
+      },
+      {
+        id: 2,
+        sourceAccount: 'A',
+        targetAccount: 'B',
+        amount: 10,
+        category: 'eating_out',
+        time: '2018-03-02T10:32:10.000Z'
+      }
+    ];
+    
+    expect(findDuplicateTransactions(INPUT)).toEqual([]);
+  });
+
 });
